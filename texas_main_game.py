bfcc0938390942baf0d7b2778d31b704b46c509f
子枭 <zixiao@zixiaodeMacBook-Pro.local>
2019-05-13 14:52:45 +0800

    good

--- texas_main_game.py
+++ texas_main_game.py
@@ -3,6 +3,9 @@ from texas_value import shuffle
 from texas_value import value_seven as valueCards
 import random
 
+S_ALLIN = 'allin'
+FOLD = 'fold'
+
 class Players():
 
     def __init__(self, name = None):
@@ -19,17 +22,28 @@ class Players():
     def broke(self):
         return self.chips == 0
 
-    def bet(self, other, pot, your_size = 0, other_size = 0):  ### not for fold or all-in###
+    def bet(self, other, pot, your_size = 0, other_size = 0, other_all_in = False):  ### not for fold or all-in###
         #####------------------------------------------------------------########
         #       change to try-except, replace input/output by functions         #
         #####------------------------------------------------------------########
+        if other_all_in:
+            to_call = other_size - your_size
+            if to_call > self.chips:
+                to_call = self.chips
+            status = '{} goes all-in. Do you want to call or fold?'.format(other.name)
+            instructions = '''enter 0 for fold, {0} for call'''.format(to_call)
+            bet_size = int(input('{} {}'.format(status, instructions)))
+            return bet_size
+
         if other_size - your_size > 0:
             to_call = other_size - your_size
             to_raise = other_size + to_call
+            if to_raise >= self.chips:
+                to_raise = self.chips
             all_in = self.chips
 
             status = 'Do you want to call, raise, or fold?'
-            instructions = '''enter 0 for fold, {0} or for call, {1} for all-in or anything between {2} and {1} for raise '''\
+            instructions = '''enter 0 for fold, {0} for call, {1} for all-in or anything between {2} and {1} for raise '''\
                 .format(to_call, all_in, to_raise)
 
             if all_in < to_call:
@@ -51,6 +65,9 @@ class Players():
                 elif self.chips < bet_size:
                     print("Warning, you have only {} chips left. Wrong input again will be treated as a Fold".format(self.chips))
                     bet_size = int(input("{} bets {}, you need {} to call. {} {}".format(other.name, other_size, to_call, status,instructions)))
+
+            if bet_size == 0:
+                return 0
 
         else:
             all_in = self.chips
@@ -86,14 +103,16 @@ pot = Pot()
 player1 = Players()
 player2 = Players()
 
-deck=shuffle()
-random.shuffle(deck)
+
 
 
 
 
 
 def actions(first, second, round, pot, sb = 1, bb = 2):
+    if first.all_in() or second.all_in():
+        return 0
+
     if round == 0:
         size1 = sb
         size2 = bb
@@ -111,41 +130,47 @@ def actions(first, second, round, pot, sb = 1, bb = 2):
         if size1 == size2:
             break
 
+
+
         size2 += second.bet(first, pot, size2, size1)
 
 
+def game(player1, player2, pot):
+    deck=shuffle()
+    random.shuffle(deck)
 
-print("player 1 gets",deck[0],deck[1],"\t\t\tplayer 2 gets",deck[7],deck[8])
-actions(player2, player1, 0, pot)
+    print("player 1 gets",deck[0],deck[1],"\t\t\tplayer 2 gets",deck[7],deck[8])
+    actions(player2, player1, 0, pot)
 
-print(deck[2],deck[3],deck[4],"\t\t\t\t",deck[2],deck[3],deck[4])
-actions(player1, player2, 1, pot)
+    print(deck[2],deck[3],deck[4],"\t\t\t\t",deck[2],deck[3],deck[4])
+    actions(player1, player2, 1, pot)
 
-print(deck[5],"\t\t\t\t\t",deck[5])
-actions(player1, player2, 2, pot)
+    print(deck[5],"\t\t\t\t\t",deck[5])
+    actions(player1, player2, 2, pot)
 
-print(deck[6],"\t\t\t\t\t",deck[6])
-actions(player1, player2, 3, pot)
+    print(deck[6],"\t\t\t\t\t",deck[6])
+    actions(player1, player2, 3, pot)
 
 
-v1=valueCards(deck[0:7])
-print("for player 1, ",v1)
-v2=valueCards(deck[2:9])
-print("for player 2, ",v2)
+    v1=valueCards(deck[0:7])
+    print("for player 1, ",v1)
+    v2=valueCards(deck[2:9])
+    print("for player 2, ",v2)
 
-if v1>v2:
-    print("player 1 wins!")
-    player1.win(pot.size)
-    pot.reset()
+    if v1>v2:
+        print("player 1 wins!")
+        player1.win(pot.size)
+        pot.reset()
 
-elif v1<v2:
-    print("player 2 wins!")
-    player2.win(pot.size)
-    pot.reset()
+    elif v1<v2:
+        print("player 2 wins!")
+        player2.win(pot.size)
+        pot.reset()
 
-else:
-    print("it is a draw.")
-    player1.win(pot.size//2)
-    player2.win(pot.size//2)
-    pot.reset()
+    else:
+        print("it is a draw.")
+        player1.win(pot.size//2)
+        player2.win(pot.size//2)
+        pot.reset()
 
+game(player1, player2, pot)
