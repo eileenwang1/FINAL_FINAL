468542633c2060ab48b3a73bddfbe307fa2772de
tw1666@nyu.edu <6Cantabile6>
2019-05-15 21:47:01 +0800

    success with little bug

--- chat_cmdl_client.py
+++ chat_cmdl_client.py
@@ -1,12 +1,7 @@
 
 from chat_client_class import *
 import ui3
-from tkinter import *
 import threading
-from multiprocessing.pool import ThreadPool
-
-pool = ThreadPool(processes=1)
-
 
 def main():
     import argparse
@@ -14,12 +9,13 @@ def main():
     parser.add_argument('-d', type=str, default=None, help='server IP addr')
     args = parser.parse_args()
 
-    client_ui = ui3.main1()
-
-    client = Client(args, client_ui)
-
-    x1 = threading.Thread(target=client.run_chat)
+    client_ui = ui3.main1() #create an instance of the ui class
+    client = Client(args, client_ui)    #create an instance of the client class
+    x1 = threading.Thread(target=client.run_chat)   #put run_chat into threading
+    #x1.daemon = True
     x1.start()
+    #client_ui.update()
+    client_ui.display()
     client_ui.mainloop()
 
 if __name__ == '__main__':
