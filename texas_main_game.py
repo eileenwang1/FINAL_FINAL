cdc423ad44feaced1346730e18749c334a81d93f
子枭 <zixiao@zixiaodeMacBook-Pro.local>
2019-05-15 16:41:28 +0800

    texas_poker_done, need to change input/output to fit into the chat system

--- texas_main_game.py
+++ texas_main_game.py
@@ -11,10 +11,19 @@ class Players():
     def __init__(self, name = None):
         self.chips = 100
         self.name = name
+        self.status = ''
+        self.instructions = ''
 
     def win(self, pot_size):
         self.chips += pot_size
-        self.out_put("{} win {}, and your money now is {}".format(self.name, pot_size, self.chips))
+        self.out_put("Congratulations! you win {}, and your money now is {}".format(pot_size//2, self.chips))
+
+    def lose(self, pot_size):
+        self.out_put("you lose {} during the hand. Your money now is {}".format(pot_size//2, self.chips))
+
+    def split(self, pot_size):
+        self.chips += pot_size//2
+        self.out_put("You split the money. Your money now is {}".format(self.chips))
 
     def all_in(self):
         return self.chips == 0
@@ -30,9 +39,9 @@ class Players():
             to_call = other_size - your_size
             if to_call > self.chips:
                 to_call = self.chips
-            status = '{} goes all-in. Do you want to call or fold?'.format(other.name)
-            instructions = '''enter 0 for fold, {0} for call'''.format(to_call)
-            bet_size = self.get_bet_size(status, instructions)
+            self.status = '{} goes all-in. Do you want to call or fold?'.format(other.name)
+            self.instructions = '''enter 0 for fold, {0} for call'''.format(to_call)
+            bet_size = self.get_bet_size()
             return bet_size
 
         if other_size - your_size > 0:
@@ -42,38 +51,38 @@ class Players():
                 to_raise = self.chips
             all_in = self.chips
 
-            status = 'Do you want to call, raise, or fold?'
-            instructions = '''enter 0 for fold, {0} for call, {1} for all-in or anything between {2} and {1} for raise '''\
+            self.status = 'Do you want to call, raise, or fold?'
+            self.instructions = '''enter 0 for fold, {0} for call, {1} for all-in or anything between {2} and {1} for raise '''\
                 .format(to_call, all_in, to_raise)
 
             if all_in < to_call:
-                status = 'Do you want to call (all-in), or fold?'
-                instructions = '''enter 0 for fold, or {} for all-in'''.format(all_in)
-                bet_size = self.get_bet_size(status, instructions)
+                self.status = 'Do you want to call (all-in), or fold?'
+                self.instructions = '''enter 0 for fold, or {} for all-in'''.format(all_in)
+                bet_size = self.get_bet_size()
                 if bet_size != 0 and bet_size != all_in:
                     self.out_put("Warning, you can only call or all-in now. Wrong input again will be treated as a Fold".format(to_call))
-                    bet_size = self.get_bet_size(status, instructions)
+                    bet_size = self.get_bet_size()
 
             else:
-                bet_size = self.get_bet_size(status, instructions)
+                bet_size = self.get_bet_size()
                 if 0 < bet_size < to_call:
                     self.out_put("Warning, you need to bet at least {} to call. Wrong input again will be treated as a Fold".format(to_call))
-                    bet_size = self.get_bet_size(status, instructions)
+                    bet_size = self.get_bet_size()
                 elif to_call < bet_size < to_raise:
                     self.out_put("Warning, you need to bet at least {} to raise. Wrong input again will be treated as a Fold".format(to_raise))
-                    bet_size = self.get_bet_size(status, instructions)
+                    bet_size = self.get_bet_size()
                 elif self.chips < bet_size:
                     self.out_put("Warning, you have only {} chips left. Wrong input again will be treated as a Fold".format(self.chips))
-                    bet_size = self.get_bet_size(status, instructions)
+                    bet_size = self.get_bet_size()
 
             if bet_size == 0:
                 return FOLD
 
         else:
             all_in = self.chips
-            status = 'Do you want to check or bet?'
-            instructions = '''enter 0 for check, or no more than {} for bet '''.format(all_in)
-            bet_size = self.get_bet_size(status, instructions)
+            self.status = 'Do you want to check or bet?'
+            self.instructions = '''enter 0 for check, or no more than {} for bet '''.format(all_in)
+            bet_size = self.get_bet_size()
         self.chip_change(bet_size, pot)
         return bet_size
 
@@ -90,10 +99,21 @@ class Players():
 
     def input_instructions(self, message):
         user_input = input(message)
-        return user_input
+        try:
+            user_input = int(user_input)
+        except:
+            self.out_put('You have to enter an integer. Invalid input again would be treated as a Fold')
+            user_input = self.input_instructions(message)
+            try:
+                user_input = int(user_input)
+            except:
+                user_input = 0
 
-    def get_bet_size(self, status, instructions):
-        message = '{} {}'.format(status, instructions)
+        finally:
+           return user_input
+
+    def get_bet_size(self):
+        message = '{} {}'.format(self.status, self.instructions)
         return self.input_instructions(message)
 
 
@@ -110,18 +130,8 @@ class Pot():
     def __repr__(self):
         return str(self.size)
 
-
-pot = Pot()
-player1 = Players("Alice")
-player2 = Players("Bob")
-
-
-
-
-
-
-
 def actions(first, second, round, pot, sb = 1, bb = 2):
+
     if first.all_in() or second.all_in():
         return 0
 
@@ -136,14 +146,14 @@ def actions(first, second, round, pot, sb = 1, bb = 2):
 
     current = first.bet(second, pot, size1, size2)
     if current == FOLD:
-        player2.win(pot.size)
+        second.win(pot.size)
         return FOLD
     size1 += current
     # bet(self, other, pot, my_size = 0, other_size = 0):
 
     current = second.bet(first, pot, size2, size1)
     if current == FOLD:
-        player1.win(pot.size)
+        first.win(pot.size)
         return FOLD
     size2 += current
 
@@ -151,7 +161,7 @@ def actions(first, second, round, pot, sb = 1, bb = 2):
 
         current = first.bet(second, pot, size1, size2)
         if current == FOLD:
-            player2.win(pot.size)
+            second.win(pot.size)
             return FOLD
         size1 += current
         # bet(self, other, pot, my_size = 0, other_size = 0):
@@ -160,7 +170,7 @@ def actions(first, second, round, pot, sb = 1, bb = 2):
 
         current = second.bet(first, pot, size2, size1)
         if current == FOLD:
-            player1.win(pot.size)
+            first.win(pot.size)
             return FOLD
         size2 += current
 
@@ -171,44 +181,75 @@ def game(player1, player2, pot):
     deck = shuffle()
     random.shuffle(deck)
 
-    self.out_put("player 1 gets",deck[0],deck[1],"\t\t\tplayer 2 gets",deck[7],deck[8])
+    player1_card = 'Your hand: {} {}'.format(deck[0], deck[1])
+    player2_card = 'Your hand: {} {}'.format(deck[7], deck[8])
+    player1.out_put(player1_card)
+    player2.out_put(player2_card)
+
     result = actions(player2, player1, 0, pot)
     if result == FOLD:
         return
 
-    self.out_put(deck[2],deck[3],deck[4],"\t\t\t\t",deck[2],deck[3],deck[4])
+    common_msg = 'Communal cards: {} {} {}'.format(deck[2], deck[3], deck[4])
+    player1_msg = '{} {}'.format(common_msg, player1_card)
+    player2_msg = '{} {}'.format(common_msg, player2_card)
+    player1.out_put(player1_msg)
+    player2.out_put(player2_msg)
+
+
     actions(player1, player2, 1, pot)
     if result == FOLD:
         return
 
-    self.out_put(deck[5],"\t\t\t\t\t",deck[5])
+    common_msg = 'Communal cards: {} {} {} {} '.format(deck[2], deck[3], deck[4], deck[5])
+    player1_msg = '{} {}'.format(common_msg, player1_card)
+    player2_msg = '{} {}'.format(common_msg, player2_card)
+    player1.out_put(player1_msg)
+    player2.out_put(player2_msg)
+
+
     actions(player1, player2, 2, pot)
     if result == FOLD:
         return
 
-    self.out_put(deck[6],"\t\t\t\t\t",deck[6])
+    common_msg = 'Communal cards: {} {} {} {} {} '.format(deck[2], deck[3], deck[4], deck[5], deck[6])
+    player1_msg = '{} {}'.format(common_msg, player1_card)
+    player2_msg = '{} {}'.format(common_msg, player2_card)
+    player1.out_put(player1_msg)
+    player2.out_put(player2_msg)
+
     actions(player1, player2, 3, pot)
     if result == FOLD:
         return
 
 
     v1=valueCards(deck[0:7])
-    self.out_put("for player 1, ",v1)
     v2=valueCards(deck[2:9])
-    self.out_put("for player 2, ",v2)
+    player1_msg = 'You got {}. {} got {}'.format(v1[2], player2.name, v2[2])
+    player2_msg = 'You got {}. {} got {}'.format(v2[2], player1.name, v1[2])
+
+    player1.out_put(player1_msg)
+    player2.out_put(player2_msg)
 
     if v1>v2:
-        self.out_put("player 1 wins!")
         player1.win(pot.size)
+        player2.lose(pot.size)
 
     elif v1<v2:
-        self.out_put("player 2 wins!")
         player2.win(pot.size)
+        player1.lose(pot.size)
 
     else:
-        self.out_put("it is a draw.")
-        player1.win(pot.size//2)
-        player2.win(pot.size//2)
+        self.out_put("It is a draw.")
+        player1.split(pot.size)
+        player2.split(pot.size)
 
     pot.reset()
-game(player1, player2, pot)
+
+def game_start(name_1, name_2):
+    pot = Pot()
+    player1 = Players(name_1)
+    player2 = Players(name_2)
+    game(player1, player2, pot)
+
+game_start('Alice', 'Bob')
